3497beeb47c82c416461d496a3ffb719d0bc7d48
Jonathanrodrigr12 <53848036+Jonathanrodrigr12@users.noreply.github.com>
2021-11-29 14:35:12 -0500

    Update carta.py

--- projectMemora/project/utilidad/cartas/carta.py
+++ projectMemora/project/utilidad/cartas/carta.py
@@ -5,6 +5,10 @@ Clase Carta donde se realiza la definición de las funciones y propiedades
 que ayudan a realizar creación de las cartas para el juego memora
 '''
 class Carta:
+    
+    '''
+    Constructor donde se incializa las clases y propiedades para la creación de las cartas
+    '''
     def __init__(self, path:str):
         self.valor = 0
         self.posicon = 0
