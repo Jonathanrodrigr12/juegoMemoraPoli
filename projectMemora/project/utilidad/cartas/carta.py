b3e2d9ab213b5ec30274e98a622f90c7cfd1054f
Jonathanrodrigr12 <53848036+Jonathanrodrigr12@users.noreply.github.com>
2021-11-29 14:34:26 -0500

    Update carta.py

--- projectMemora/project/utilidad/cartas/carta.py
+++ projectMemora/project/utilidad/cartas/carta.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+'''
+Clase Carta donde se realiza la definición de las funciones y propiedades
+que ayudan a realizar creación de las cartas para el juego memora
+'''
 class Carta:
     def __init__(self, path:str):
         self.valor = 0
